19a49463db516d7476e3618f296f11855523c6a4
hjoo830 <hjoo830@naver.com>
2024-09-04 03:33:23 +0900

    ✨ Feat: Add conditional rendering for text by type in Section.js

--- src/component/Section.js
+++ src/component/Section.js
@@ -1,9 +1,22 @@
 import Memo from "./Memo";
 
 function Section({ type, memos }) {
+  const getText = () => {
+    if (type === "Keep") {
+      return "잘하고 있는 점";
+    } else if (type === "Problem") {
+      return "문제점";
+    } else if (type === "Try") {
+      return "시도할 점";
+    } 
+  };
+  
   return (
     <div>
-      <h3>{type}</h3>
+      <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+        <h3>{type}</h3>
+        <p style={{ fontSize: "small" }}>{getText()}</p>
+      </div>
       <div
         style={{
           width: "95%",
