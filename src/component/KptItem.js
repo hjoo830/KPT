5b3ca2dc01ce33363874a3f97b417924a6517f42
hjoo830 <hjoo830@naver.com>
2024-09-04 03:22:23 +0900

    ✨ Feat: KptItem UI

---
+++ src/component/KptItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Section from "./Section";
+
+function KptItem({ kpt }) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gap: "10px",
+      }}
+    >
+      {kpt.map((section) => (
+        <div
+          key={section.type}
+          style={{
+            gridRow: section.type === "Try" ? "span 2" : "span 1",
+            backgroundColor:
+              section.type === "Keep"
+                ? "#ffc7c2"
+                : section.type === "Try"
+                ? "#bce3ff"
+                : "#fcd19c", 
+            padding: "0 20px",
+            borderRadius: "5px",
+          }}
+        >
+          <Section type={section.type} memos={section.memo} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default KptItem;
